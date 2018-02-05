719046751ae0b715e3bb03f914108a8f65660c98
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-05 22:03:38 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0 
+ array2 = []
+ while i < collection.size
+   torf = yield collection[i]
+   if torf == true 
+     array2.push(collection[i])
+   end 
+   i += 1 
+ end 
+ array2
 end
