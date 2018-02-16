42c64990b34ca823e9e87523299a81d58385a3fe
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 23:27:51 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if collection.size != 0
+    i = 0
+    array = []
+    while i < collection.length
+      if yield collection[i]
+        array << collection[i]
+      end
+      i = i + 1
+    end
+  else
+    puts "empty"
+  end
+  array
 end
